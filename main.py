e23688c143c43eee65a2de83f2f01b293e520364
Rishav Mitra <rishavmitra10@gmail.com>
2021-06-19 12:54:00 +0000

    Rewrite the code for a more generic  application

--- main.py
+++ main.py
@@ -1,52 +1,48 @@
 import hashlib
+import time
+import json
 
 class Block():
-	def __init__(self, prev_hash, transactions):
+	def __init__(self, index, prev_hash, transactions):
+		self.index = index
+		self.timestamp = 'time.time()' # We use a string so that the hash does not change everytime we run the program
 		self.prev_hash = prev_hash
 		self.transactions = transactions
+		self.hash = self.calculate_hash()
 
-		self.block_data = '-'.join(self.transactions) + '-' + self.prev_hash
-		self.block_hash = hashlib.sha256(self.block_data.encode()).hexdigest()
+	def calculate_hash(self):
+		block_data = f'{self.index}{self.timestamp}{self.prev_hash}{json.dumps(self.transactions)}'
+		return hashlib.sha256(block_data.encode()).hexdigest()
 
-	def get_block_hash(self):
-		return self.block_hash
+	def __repr__(self):
+		return json.dumps({
+			'index' : self.index,
+			'timestamp' : self.timestamp,
+			'previous hash' : self.prev_hash,
+			'transactions' : self.transactions,
+			'hash' : self.hash
+		}, indent = 4)
 
 class Blockchain():
 	def __init__(self):
-		self.block_list = []
+		self.chain = []
 
-	def create_genesis_block(self, transactions):
-		genesis_block = Block("Initial block", transactions)
-		self.block_list.append(genesis_block)
+	def create_genesis_block(self):
+		genesis_block = Block(0, "0", "Initial block")
+		self.chain.append(genesis_block)
+
+	def get_last_block(self):
+		return self.chain[-1]
 
 	def create_block(self, transactions):
-		new_block = Block(self.block_list[-1].get_block_hash(), transactions)
-		self.block_list.append(new_block)
-
-	def get_blockchain_info(self):
-		for idx, block in enumerate(self.block_list):
-			print(idx, block.get_block_hash())
-
-# Let's take some sample transactions
-t1 = "John sends 3.5 coins to Mark"
-t2 = "Michael sends 2.2 coins to Harry"
-t3 = "Mark sends 5.5 coins to Theo"
-t4 = "Michael sends 2.5 coins to Theo"
-t5 = "Harry sends 3.4 coins to Dora"
-t6 = "Dora sends 4.2 coins to George"
-t7 = "Theo sends 4.8 coins to John"
-t8 = "Dora sends 5.2 coins to Harry"
+		new_block = Block(self.get_last_block().index + 1, self.get_last_block().hash, transactions)
+		self.chain.append(new_block)
 
 blockchain = Blockchain()
 
-blockchain.create_genesis_block([t1, t2])
-blockchain.create_block([t3, t4])
-blockchain.create_block([t5, t6])
-blockchain.create_block([t7, t8])
+blockchain.create_genesis_block()
+blockchain.create_block({ 'sender' : "1", 'recipient' : "Rishav", 'amount' : 4 })
+blockchain.create_block({ 'sender' : "2", 'recipient' : "Sumedha", 'amount' : 6 })
+blockchain.create_block({ 'sender' : "3", 'recipient' : "Tanya", 'amount' : 3 })
 
-blockchain.get_blockchain_info()
-
-'''
-It can be clearly observed that changing some data in the transactions changes the
-hashes of the blocks (try it!) and thus destroy the integrity of the blockchain
-'''
+print(blockchain.chain)
